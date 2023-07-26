a4db8b245245fe596f5b7bf9ed9d959c0d7dd95d
Pratya0011 <pratya0011@gmail.com>
2023-07-26 23:26:58 +0530

    commit

--- app/src/Components/request.js
+++ app/src/Components/request.js
@@ -1,46 +1,46 @@
 export const signup = {
-    signUpUrl : 'http://localhost:8080/user/signup',
-    loginUrl  :'http://localhost:8080/user/login',
+    signUpUrl : 'https://flixxit-server-9v89.onrender.com/user/signup',
+    loginUrl  :'https://flixxit-server-9v89.onrender.com/user/login',
 }
 
 export const homeRequest = {
-    topRatedFlixxit: `http://localhost:8080/user/toprated`,
-    popularFlixxit:`http://localhost:8080/user/popular`,
-    toptenFlixxit:`http://localhost:8080/user/topten`,
-    documentaryFlixxit:`http://localhost:8080/user/documentary`,
-    recomendedFlixxit:`http://localhost:8080/user/recomended`,
-    getTitle:`http://localhost:8080/user/getTitle`
+    topRatedFlixxit: `https://flixxit-server-9v89.onrender.com/user/toprated`,
+    popularFlixxit:`https://flixxit-server-9v89.onrender.com/user/popular`,
+    toptenFlixxit:`https://flixxit-server-9v89.onrender.com/user/topten`,
+    documentaryFlixxit:`https://flixxit-server-9v89.onrender.com/user/documentary`,
+    recomendedFlixxit:`https://flixxit-server-9v89.onrender.com/user/recomended`,
+    getTitle:`https://flixxit-server-9v89.onrender.com/user/getTitle`
 }
 
 export const movieRequest = {
-    topRatedMovies: `http://localhost:8080/user/topRatedMovies`,
-    popularMovies: `http://localhost:8080/user/popularMovies`,
-    thrillerMovies: `http://localhost:8080/user/thrillerMovies`,
-    crimeMovies: `http://localhost:8080/user/crimeMovies`,
-    dramaMovies: `http://localhost:8080/user/dramaMovies`,
-    actionMovies: `http://localhost:8080/user/actionMovies`,
-    adventureMovies: `http://localhost:8080/user/adventureMovies`,
-    horrorMovies: `http://localhost:8080/user/horrorMovies`,
-    comedyMovies: `http://localhost:8080/user/comedyMovies`,
-    romanceMovies: `http://localhost:8080/user/romanceMovies`,
-    documentaryMovies: `http://localhost:8080/user/documentaryMovies`
+    topRatedMovies: `https://flixxit-server-9v89.onrender.com/user/topRatedMovies`,
+    popularMovies: `https://flixxit-server-9v89.onrender.com/user/popularMovies`,
+    thrillerMovies: `https://flixxit-server-9v89.onrender.com/user/thrillerMovies`,
+    crimeMovies: `https://flixxit-server-9v89.onrender.com/user/crimeMovies`,
+    dramaMovies: `https://flixxit-server-9v89.onrender.com/user/dramaMovies`,
+    actionMovies: `https://flixxit-server-9v89.onrender.com/user/actionMovies`,
+    adventureMovies: `https://flixxit-server-9v89.onrender.com/user/adventureMovies`,
+    horrorMovies: `https://flixxit-server-9v89.onrender.com/user/horrorMovies`,
+    comedyMovies: `https://flixxit-server-9v89.onrender.com/user/comedyMovies`,
+    romanceMovies: `https://flixxit-server-9v89.onrender.com/user/romanceMovies`,
+    documentaryMovies: `https://flixxit-server-9v89.onrender.com/user/documentaryMovies`
 }
 
 export const tvRequest = {
-    topRatedTv: `http://localhost:8080/user/topRatedTv`,
-    popularTv: `http://localhost:8080/user/popularTv`,
-    crimeTv:   `http://localhost:8080/user/crimeTv`,
-    dramaTv: `http://localhost:8080/user/dramaTv`,
-    comedyTv: `http://localhost:8080/user/comedyTv`,
-    mysteryTv: `http://localhost:8080/user/mysteryTv`,
-    actionadventureTv: `http://localhost:8080/user/action&advntureTv`,
-    documentaryTv: `http://localhost:8080/user/documentaryTv`
+    topRatedTv: `https://flixxit-server-9v89.onrender.com/user/topRatedTv`,
+    popularTv: `https://flixxit-server-9v89.onrender.com/user/popularTv`,
+    crimeTv:   `https://flixxit-server-9v89.onrender.com/user/crimeTv`,
+    dramaTv: `https://flixxit-server-9v89.onrender.com/user/dramaTv`,
+    comedyTv: `https://flixxit-server-9v89.onrender.com/user/comedyTv`,
+    mysteryTv: `https://flixxit-server-9v89.onrender.com/user/mysteryTv`,
+    actionadventureTv: `https://flixxit-server-9v89.onrender.com/user/action&advntureTv`,
+    documentaryTv: `https://flixxit-server-9v89.onrender.com/user/documentaryTv`
 }
 
 export const Watchlist={
-    getWatchlist : `http://localhost:8080/user/getWatchlist`,
-    addWatchlist : `http://localhost:8080/user/addWatchlist`,
-    deleteWatchlist : `http://localhost:8080/user/deleteWatchlist`
+    getWatchlist : `https://flixxit-server-9v89.onrender.com/user/getWatchlist`,
+    addWatchlist : `https://flixxit-server-9v89.onrender.com/user/addWatchlist`,
+    deleteWatchlist : `https://flixxit-server-9v89.onrender.com/user/deleteWatchlist`
 }
 
 export const searchBar = {
