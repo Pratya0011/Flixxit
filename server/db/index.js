d70ecab190fd5e5201e7f238bea88071efdbe3a1
Pratya0011 <pratya0011@gmail.com>
2023-07-31 17:57:22 +0530

    commit

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 
 
 config()
-const connection = mongoose.connect(process.env.MONGODB,
+const connection = mongoose.connect(`mongodb+srv://pratya0011:${process.env.PASSWORD}@cluster0.o1efyoe.mongodb.net/?retryWrites=true&w=majority`,
 {
     useNewUrlParser: true,
     useUnifiedTopology: true
