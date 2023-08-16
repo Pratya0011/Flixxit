6d9db7d0b098d60910a223537565ae443c295f48
Pratya0011 <pratya0011@gmail.com>
2023-08-16 22:38:45 +0530

    player deign

--- app/src/Components/request.js
+++ app/src/Components/request.js
@@ -1,84 +1,88 @@
+const url = 'https://flixxit-server-9v89.onrender.com'
+// https://flixxit-server-9v89.onrender.com
+// http://localhost:8080
+
 export const signup = {
-    signUpUrl : 'https://flixxit-server-9v89.onrender.com/user/signup',
-    loginUrl  :'https://flixxit-server-9v89.onrender.com/user/login',
-    fogotPassword : `https://flixxit-server-9v89.onrender.com/user/forgotPassword`
+    signUpUrl : `${url}/user/signup`,
+    loginUrl  :`${url}/user/login`,
+    fogotPassword : `${url}/user/forgotPassword`
 }
 
 export const homeRequest = {
-    topRatedFlixxit: `https://flixxit-server-9v89.onrender.com/user/toprated`,
-    popularFlixxit:`https://flixxit-server-9v89.onrender.com/user/popular`,
-    toptenFlixxit:`https://flixxit-server-9v89.onrender.com/user/topten`,
-    documentaryFlixxit:`https://flixxit-server-9v89.onrender.com/user/documentary`,
-    recomendedFlixxit:`https://flixxit-server-9v89.onrender.com/user/recomended`,
-    getTitle:`https://flixxit-server-9v89.onrender.com/user/getTitle`
+    topRatedFlixxit: `${url}/user/toprated`,
+    popularFlixxit:`${url}/user/popular`,
+    toptenFlixxit:`${url}/user/topten`,
+    documentaryFlixxit:`${url}/user/documentary`,
+    recomendedFlixxit:`${url}/user/recomended`,
+    getTitle:`${url}/user/getTitle`
 }
 
 export const movieRequest = {
-    topRatedMovies: `https://flixxit-server-9v89.onrender.com/user/topRatedMovies`,
-    popularMovies: `https://flixxit-server-9v89.onrender.com/user/popularMovies`,
-    thrillerMovies: `https://flixxit-server-9v89.onrender.com/user/thrillerMovies`,
-    crimeMovies: `https://flixxit-server-9v89.onrender.com/user/crimeMovies`,
-    dramaMovies: `https://flixxit-server-9v89.onrender.com/user/dramaMovies`,
-    actionMovies: `https://flixxit-server-9v89.onrender.com/user/actionMovies`,
-    adventureMovies: `https://flixxit-server-9v89.onrender.com/user/adventureMovies`,
-    horrorMovies: `https://flixxit-server-9v89.onrender.com/user/horrorMovies`,
-    comedyMovies: `https://flixxit-server-9v89.onrender.com/user/comedyMovies`,
-    romanceMovies: `https://flixxit-server-9v89.onrender.com/user/romanceMovies`,
-    documentaryMovies: `https://flixxit-server-9v89.onrender.com/user/documentaryMovies`
+    topRatedMovies: `${url}/user/topRatedMovies`,
+    popularMovies: `${url}/user/popularMovies`,
+    thrillerMovies: `${url}/user/thrillerMovies`,
+    crimeMovies: `${url}/user/crimeMovies`,
+    dramaMovies: `${url}/user/dramaMovies`,
+    actionMovies: `${url}/user/actionMovies`,
+    adventureMovies: `${url}/user/adventureMovies`,
+    horrorMovies: `${url}/user/horrorMovies`,
+    comedyMovies: `${url}/user/comedyMovies`,
+    romanceMovies: `${url}/user/romanceMovies`,
+    documentaryMovies: `${url}/user/documentaryMovies`
 }
 
 export const tvRequest = {
-    topRatedTv: `https://flixxit-server-9v89.onrender.com/user/topRatedTv`,
-    popularTv: `https://flixxit-server-9v89.onrender.com/user/popularTv`,
-    crimeTv:   `https://flixxit-server-9v89.onrender.com/user/crimeTv`,
-    dramaTv: `https://flixxit-server-9v89.onrender.com/user/dramaTv`,
-    comedyTv: `https://flixxit-server-9v89.onrender.com/user/comedyTv`,
-    mysteryTv: `https://flixxit-server-9v89.onrender.com/user/mysteryTv`,
-    actionadventureTv: `https://flixxit-server-9v89.onrender.com/user/action&advntureTv`,
-    documentaryTv: `https://flixxit-server-9v89.onrender.com/user/documentaryTv`
+    topRatedTv: `${url}/user/topRatedTv`,
+    popularTv: `${url}/user/popularTv`,
+    crimeTv:   `${url}/user/crimeTv`,
+    dramaTv: `${url}/user/dramaTv`,
+    comedyTv: `${url}/user/comedyTv`,
+    mysteryTv: `${url}/user/mysteryTv`,
+    actionadventureTv: `${url}/user/action&advntureTv`,
+    documentaryTv: `${url}/user/documentaryTv`
 }
 
 export const Watchlist={
-    getWatchlist : `https://flixxit-server-9v89.onrender.com/user/getWatchlist`,
-    addWatchlist : `https://flixxit-server-9v89.onrender.com/user/addWatchlist`,
-    deleteWatchlist : `https://flixxit-server-9v89.onrender.com/user/deleteWatchlist`
+    getWatchlist : `${url}/user/getWatchlist`,
+    addWatchlist : `${url}/user/addWatchlist`,
+    deleteWatchlist : `${url}/user/deleteWatchlist`
 }
 
 export const searchBar = {
-    getSearch: `https://flixxit-server-9v89.onrender.com/user/search`
+    getSearch: `${url}/user/search`
 }
 
 export const subscribitionPlan = {
-    getAllSubsPlans: "https://flixxit-server-9v89.onrender.com/user/getPlan",
-    checkOut:"https://flixxit-server-9v89.onrender.com/user/checkout",
-    paymentVerification:"https://flixxit-server-9v89.onrender.com/user/paymentverification",
-    getkey:"https://flixxit-server-9v89.onrender.com/user/getKey",
-    updatePaymentStatus:"https://flixxit-server-9v89.onrender.com/user/updatePaymentStatus"
+    getAllSubsPlans: `${url}/user/getPlan`,
+    checkOut:`${url}/user/checkout`,
+    paymentVerification:`${url}/user/paymentverification`,
+    getkey:`${url}/user/getKey`,
+    updatePaymentStatus:`${url}/user/updatePaymentStatus`
 } 
 
 export const getuser = {
-    getUserById : "https://flixxit-server-9v89.onrender.com/admin/getUser",
-    getallUser : "https://flixxit-server-9v89.onrender.com/admin/getallusers",
-    getSubscribedUser: "https://flixxit-server-9v89.onrender.com/admin/getsubscribed",
-    getAmount : "https://flixxit-server-9v89.onrender.com/admin/getamount",
-    favgenre : `https://flixxit-server-9v89.onrender.com/user/genre`
+    getUserById : `${url}/admin/getUser`,
+    getallUser : `${url}/admin/getallusers`,
+    getSubscribedUser: `${url}/admin/getsubscribed`,
+    getAmount : `${url}/admin/getamount`,
+    favgenre : `${url}/user/genre`
 }
 
 export const commentsRequest = {
-    getComments: `https://flixxit-server-9v89.onrender.com/user/comments`,
-    postComments: `https://flixxit-server-9v89.onrender.com/user/addcomment`
+    getComments: `${url}/user/comments`,
+    postComments: `${url}/user/addcomment`
 }
 
 export const videoRoutes = {
-    fetchVideo : `https://flixxit-server-9v89.onrender.com/user/video`
+    fetchVideo : `${url}/user/video`
 }
 
 export const historyRoutes = {
-    addHistory :`https://flixxit-server-9v89.onrender.com/user/addhistory`,
-    getHistory : `https://flixxit-server-9v89.onrender.com/user/gethistory`
+    addHistory :`${url}/user/addhistory`,
+    getHistory : `${url}/user/gethistory`
 }
 export const likes = {
-    likes:`https://flixxit-server-9v89.onrender.com/user/likes`,
-    dislikes:`https://flixxit-server-9v89.onrender.com/user/dislikes`,
-    getLikes: `https://flixxit-server-9v89.onrender.com/user/getlike`
+    likes:`${url}/user/likes`,
+    dislikes:`${url}/user/dislikes`,
+    getLikes: `${url}/user/getlike`
 }
